3f3a94e05b838964011389d85e3c176f7eb8e121
coherent17 <mnb51817@gmail.com>
2021-08-23 13:31:49 +0800

    makefile ex2

---
+++ makefile_ex2/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "tool.h"
+
+int main(){
+    int array[5]={1,8,3,4,5};
+    int max=findMax(array, 5);
+    printf("max=%d\n", max);
+    return 0;
+}
---
+++ makefile_ex2/tool.c
@@ -0,0 +1,9 @@
+#include "tool.h"
+
+int findMax(int array[], int size){
+    int max=array[0];
+    for(int i=0;i<size;i++){
+        if(array[i]>max) max=array[i];
+    }
+    return max;
+}
